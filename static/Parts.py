60066c386a5669dde3a6e0f7902ba17989b2d06b
Boston22 <csacadamy@gmail.com>
2022-05-09 14:48:03 -0700

    updating setup for mainly used templates

--- static/Parts.py
+++ static/Parts.py
@@ -1,34 +1,49 @@
-import jinja
+import os
+from flask import Flask, url_for, render_template, request
+from flask import redirect
+from flask import session
 
-app.debug = True
+app = Flask(__name__)
+
+# In order to use "sessions",you need a "secret key".
+# This is something random you generate.
+# For more info see: https://flask.palletsprojects.com/en/1.1.x/config/#SECRET_KEY
+
+app.secret_key=os.environ["SECRET_KEY"]; #This is an environment variable.
+                                     #The value should be set in Heroku (Settings->Config Vars).
+                                     #To run locally, set in env.sh and include that file in gitignore so the secret key is not made public.
+
 
 @app.route('/')
-def home():
+def renderMain():
   return render_template('home.html')
 
+@app.route('/startOver')
+def startOver():
+    session.clear() =
+    return redirect(url_for('renderMain')) # url_for('renderMain') could be replaced with '/'
+
 @app.route('/P1')
-def page1():
+def renderPage1():
   return render_template('Page1.html')
 
 @app.route('/P2')
-def page2():
+def renderPage2():
   return render_template('Page2.html')
 
 @app.route('/Q')
-def questions():
+def renderQuestions():
   return render_template('Questions.html')
 
 @app.route('/Test')
-def testing():
+def renderTesting():
   return render_template('Test.html')
 
 
 
 
 
-@github.tokengetter
-def get_github_oauth_token():
-    return session.get('github_token')
+
 
 
 if __name__ == '__main__':
