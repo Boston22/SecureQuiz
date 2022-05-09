23006c9a262f69129ba69e2d6c4bc5b93797c241
Boston22 <97976083+Boston22@users.noreply.github.com>
2022-05-09 14:19:35 -0700

    Update Parts.py

--- static/Parts.py
+++ static/Parts.py
@@ -1,1 +1,35 @@
+import jinja
 
+app.debug = True
+
+@app.route('/')
+def home():
+  return render_template('home.html')
+
+@app.route('/P1')
+def page1():
+  return render_template('Page1.html')
+
+@app.route('/P2')
+def page2():
+  return render_template('Page2.html')
+
+@app.route('/Q')
+def questions():
+  return render_template('Questions.html')
+
+@app.route('/Test')
+def testing():
+  return render_template('Test.html')
+
+
+
+
+
+@github.tokengetter
+def get_github_oauth_token():
+    return session.get('github_token')
+
+
+if __name__ == '__main__':
+    app.run()
