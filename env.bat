e15136eeefae9a28e2a9c777553f804d73d1bb94
Boston22 <csacadamy@gmail.com>
2022-05-12 11:07:26 -0700

    12ert

--- env.bat
+++ env.bat
@@ -1,5 +1,5 @@
 set SECRET_KEY=Bike/Monkeys
-set MONGO_DBNAME=CarForm
+set MONGO_DBNAME=BikeKnowledge
 set MONGO_CONNECTION_STRING=mongodb+srv://Admin:F1vAqBaNOY0C1kRA@dswcluster.v9owr.mongodb.net/CarForm?retryWrites=true
 set GITHUB_CLIENT_ID=b255d38a4c615204774c
 set GITHUB_CLIENT_SECRET=cfcdada6ac93f7aea85e9f0319941a2368b2d714
